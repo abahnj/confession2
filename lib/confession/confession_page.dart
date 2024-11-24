04c4e63e112230e2d6dc776861b37c1968b8fa1f
Nnaemeka Abah <hello@abahnj.dev>
2024-11-24 22:27:02 +0200

    adds feature stubs

---
+++ lib/confession/confession_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConfessionPage extends StatelessWidget {
+  const ConfessionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/prayers/prayers_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrayersPage extends StatelessWidget {
+  const PrayersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
